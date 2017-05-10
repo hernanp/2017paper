1a84d6193c5efdeeb491a81ccc61b8012f16378e
Hernan <hpalombo@mail.usf.edu>
2017-05-10 19:07:36 +0200

    docs

--- test.vhd
+++ test.vhd
@@ -64,8 +64,8 @@ package test is
   --********************************************************************
   --* Warning: don't enable tests that are triggered on the same signals
   constant RUN_TEST : TEST_T := TEST(RW) or
-                                TEST(PWR);-- or
-                                --TEST(UREQ);
+                                --TEST(PWR) or
+                                TEST(UREQ);
                                 --TEST(PETERSONS);
                                 --TEST(NONE);
   --********************************************************************
